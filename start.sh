9d158a95eac6ec88ed3881bbf8a5414f8eb08aa5
Cyprian Odinaka Omenuko <comenuko@zimesfield.com>
2023-10-18 08:52:54 +0100

    Fixed mixing frontend file

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
  kill -9 $(lsof -ti:8080,3000,8081,8082)
 
- cd generator && mvn clean install
- java -jar target/generator-0.0.1-SNAPSHOT.jar & cd ../validator && mvn clean package
- java -jar target/validator-0.0.1-SNAPSHOT.jar & npm install -g serve && cd ../frontend && npm run openapi:gen && npm run build && serve -s ../build/webapp -p 8080
+ cd generator && mvn clean install && java -jar target/generator-0.0.1-SNAPSHOT.jar & cd validator && mvn clean install && java -jar target/validator-0.0.1-SNAPSHOT.jar & npm install -g serve
+ /n & sleep 10 && cd frontend && npm install && npm run openapi:gen && npm run build && serve -s ../build/webapp -p 8080
